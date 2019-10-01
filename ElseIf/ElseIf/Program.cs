af159408a9eb3ede0c1b5eee9eddfce33e0b7dde
Anssihapponen <anssi.happonen@student.saimia.fi>
2019-10-01 08:58:21 +0300

    IfTask 1 done, IfTask 2 created

---
+++ ElseIf/ElseIf/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ElseIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää käyttäjän syöttämästä luvusta, onko se positiivinen vai negatiivinen");
+            Console.Write("Syötä kokonaisluku: ");
+            int userInputNumber = int.Parse(Console.ReadLine());
+            {
+                if (userInputNumber == 0)
+                {
+                    Console.WriteLine($"Luku {userInputNumber} on nolla!");
+                }
+                else
+                {
+                    if (userInputNumber > 0)
+                    {
+                        Console.WriteLine($"Luku {userInputNumber} on positiivinen");
+                    }
+                    else
+                    {
+                        if (userInputNumber < 0)
+                        {
+                            Console.WriteLine($"Luku {userInputNumber} on negatiivinen");
+                        }
+                    }
+                
+                }
+            
+                   
+                   
+            }
+        }
+    }
+}
