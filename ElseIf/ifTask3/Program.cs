9273cd49e43a858614be439ad6dc086557590b81
Anssihapponen <anssi.happonen@student.saimia.fi>
2019-10-03 09:46:37 +0300

    ifTask 3 Done

---
+++ ElseIf/ifTask3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ifTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää onko luku positiivinen vai negatiivinen, sekä onko luku parillinen vai pariton");
+            Console.Write("Syötä luku: ");
+
+            int userInputNumber = int.Parse(Console.ReadLine());
+            if (userInputNumber == 0)
+                Console.WriteLine("Luku on nolla");
+            else
+                if (userInputNumber > 0)
+                Console.WriteLine("Luku on positiivinen");
+            else Console.WriteLine("Luku on negatiivinen");
+
+
+            if (userInputNumber % 2 ==0)
+                Console.WriteLine("Luku on parillinen");
+            else Console.WriteLine("Luku on pariton");
+
+
+        }
+    }
+}
