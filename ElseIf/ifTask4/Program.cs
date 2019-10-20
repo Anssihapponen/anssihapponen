a42afab31e865eb3577d26f5b1ebce292262879b
Anssihapponen <anssi.happonen@student.saimia.fi>
2019-10-20 23:43:21 +0300

    if task 4 done

---
+++ ElseIf/ifTask4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ifTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma lajittelee luvut nousevaan järjestykseen");
+            Console.Write("Syötä 1. luku: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Syötä 2. Luku: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("Syötä 3. luku: ");
+            int z = int.Parse(Console.ReadLine());
+
+            if (x > y)
+                if (y > z)
+                    Console.WriteLine($"{z},{y},{x}");
+                else if (x > z)
+                    Console.WriteLine($"{y},{z},{x}");
+                else Console.WriteLine($"{y},{x},{z}");
+            if (y > x)
+                if (x > z)
+                    Console.WriteLine($"{z},{x},{y}");
+                else if (y > z)
+                    Console.WriteLine($"{x},{z},{y}");
+                else Console.WriteLine($"{x},{y},{z}");
+            
+                
+
+            
+            
+            
+            
+            
+
+
+        }
+    }
+}
